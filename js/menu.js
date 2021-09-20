3525b0eacb6e251ddce8f5c7d11a4dfcf401fa15
Bruno Henrique Henrique <85626791+Brunohploureiro@users.noreply.github.com>
2021-09-20 16:58:08 +0000

    5

--- js/menu.js
+++ js/menu.js
@@ -1,15 +1,37 @@
 $(document).ready(function(){
-
     $('#btn-menu').click(function(e){
-        e.preventDefault()
-        $('#menu').toggle('slow')
+      e.preventDefault()
+      $('#menu').toggle('slow')
     })
 
-    
     $('a').click(function(e){
-        e.preventDefault()
-        
-        let url = $(this).attr('href')
-        $('#'+url).show()
+      e.preventDefault()
+
+      let url = $(this).attr('href')
+      
+      if(url == 'home'){
+         $('#'+url).show()
+         $('#about').hide()
+         $('#action').hide()
+         $('#contact').hide()
+      }
+      else if(url == 'about'){
+       $('#'+url).show()
+       $('#home').hide()
+       $('#action').hide()
+       $('#contact').hide()
+      }
+      else if(url == 'action'){
+       $('#'+url).show()
+       $('#home').hide()
+       $('#about').hide()
+       $('#contact').hide()
+      }
+      else if(url == 'contact'){
+       $('#'+url).show()
+       $('#home').hide()
+       $('#action').hide()
+       $('#about').hide()
+      }     
     })
-})
+ })
