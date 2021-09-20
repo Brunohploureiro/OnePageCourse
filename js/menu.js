7c6506c9506aa8e30827759fbac5c2f67cb481c8
Bruno Henrique Henrique <85626791+Brunohploureiro@users.noreply.github.com>
2021-09-20 14:34:50 +0000

    3

---
+++ js/menu.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+    $('#btn-menu').click(function(e){
+        e.preventDefault()
+        $('#menu').toggle('slow')
+    })
+
+    
+    $('a').click(function(e){
+        e.preventDefault()
+        
+        let url = $(this).attr('href')
+        $('#'+url).show()
+    })
+})
